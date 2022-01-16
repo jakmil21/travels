440d02939893c0160c9594d909cea73684e93c61
Jakub Miller <jakub.miller92@gmail.com>
2022-01-16 16:20:42 +0100

    Add first page object

--- src/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
+++ src/test/java/pl/seleniumdemo/tests/HotelSearchTest.java
@@ -1,7 +1,10 @@
+package pl.seleniumdemo.tests;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pl.seleniumdemo.pages.HotelSearchPage;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -10,23 +13,11 @@ public class HotelSearchTest extends BaseTest {
 
     @Test
     public void searchHotelTest() {
-        driver.findElement(By.xpath("//span[text()='Search by Hotel or City Name']")).click();
-        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
-        driver.findElement(By.xpath("//span[@class='select2-match' and text()='Dubai']")).click();
-
-        driver.findElement(By.name("checkin")).sendKeys("25/01/2022");
-        driver.findElement(By.name("checkout")).click();
-        driver.findElements(By.xpath("//td[@class='day ' and text()='30']"))
-                .stream()
-                .filter(WebElement::isDisplayed)
-                .findFirst()
-                .ifPresent(WebElement::click);
-
-        driver.findElement(By.name("travellers")).click();
-        driver.findElement(By.id("adultPlusBtn")).click();
-        driver.findElement(By.id("childPlusBtn")).click();
-
-        driver.findElement(By.xpath("//button[text()=' Search']")).click();
+        HotelSearchPage hotelSearchPage = new HotelSearchPage(driver);
+        hotelSearchPage.setCity("Dubai");
+        hotelSearchPage.setDates("25/01/2022", "28/01/2022");
+        hotelSearchPage.setTravellers();
+        hotelSearchPage.performSearch();
 
         List<String> hotelNames = driver.findElements(By.xpath("//h4[contains(@class, 'list_title')]//b")).stream()
                                                                                         .map(el -> el.getAttribute("textContent"))
--- src/test/java/pl/seleniumdemo/tests/SignUpTest.java
+++ src/test/java/pl/seleniumdemo/tests/SignUpTest.java
@@ -1,3 +1,4 @@
+package pl.seleniumdemo.tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
